c801f6ef7f1041c22b1824de4363df376ce194c0
PrasadLalu <lalumahato020@gmail.com>
2025-04-23 17:43:06 +0530

    Project app configure

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,45 @@
 import 'module-alias/register';
 import http from 'http';
+import path from 'path';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
 import * as dotenv from 'dotenv';
-import express, { Express } from 'express';
-import apiRoutes from './routes';
+import express, { Application } from 'express';
 import logger from '@logger';
+import apiRoutes from './routes';
 
 // Load env vars
 dotenv.config();
 
 // Initial app
-const app: Express = express();
+const app: Application = express();
 
 // Initial port
-const PORT: number = 9000;
+const PORT: number = parseInt(process.env.PORT || '9000', 10);
+
+// API logger
+// API logger
+if (process.env.NODE_ENV === 'development') {
+    const myFormat = '[:date[clf]] ":method :url" :status :res[content-length] - :response-time ms';
+    app.use(morgan(myFormat));
+}
 
 // middlewares
+app.use(cors());
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 // Mount api routes
 app.use('/api/v1', apiRoutes);
 
-// Create server and listen to server
-const server = http.createServer(app);
-server.listen(PORT, () => {
-    logger.info(`Server running on port: ${PORT}`);
-});
+// Create server and listen
+http
+    .createServer(app)
+    .listen(PORT, () => logger.info(`Application running on port:${PORT}`));
